5ce20fc05dea4f03d3e08a2a0c237f97f6e707e6
elmyrza1 <elmyrza217@gmail.com.com>
2025-05-19 13:08:27 +0600

    отрефактори вьюшку и некоторые страницы

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
-from urllib import request
 from django.contrib.auth import login, logout
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import  redirect
 from django.views import View
 from django.views.generic import TemplateView
 
@@ -9,43 +8,56 @@ from users.models import CustomUser
 
 
 class UserRegistrationView(TemplateView):
+    """
+    вьюшка для того что бы показывать страницу лоя регистрации
+    """
     template_name = 'register.html'
 
 
 class MakeUserRegistrationView(View):
+    """
+    вьюшка для того чтобы регистрировать пользователя
+    """
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         data = request.POST
-
         password1 = data['password1']
         password2 = data['password2']
 
         if password1 != password2:
-            return HttpResponse('Вы неправильно ввели пароль')
+            messages.error(request, "Пароли не совпадают.")
+            return redirect('register-url')
 
-        first_name = data['firstName']
-        last_name = data['lastName']
+        first_name = data['first_name']
+        last_name = data['last_name']
         email = data['email']
 
         if CustomUser.objects.filter(email=email).exists():
-            return HttpResponse('Пользователь с таким email уже зарегистрирован')
+            messages.error(request, "Пользователь с таким email уже существует.")
+            return redirect('register-url')
 
         user = CustomUser.objects.create_user(
             email=email, password1=password1,
-            first_name=first_name, last_name=last_name,
-            )
+            first_name=first_name, last_name=last_name
+        )
 
         login(request, user)
+
         return redirect('home-url')
 
 
-
 class LoginListView(TemplateView):
+    """
+    вьюшка для показа страницы для логина
+    """
     template_name = 'Login.html'
 
 
 class MakeLoginView(View):
-    def post(self, request, *args, **kwargs):
+    """
+    вьюшка для того что бы пользователь мог входить в акк
+    """
+    def post(self, request,):
         data = request.POST
         email = data['email']
         password = data['password']
@@ -53,19 +65,21 @@ class MakeLoginView(View):
         try:
             user = CustomUser.objects.get(email=email)
         except CustomUser.DoesNotExist:
-            return HttpResponse('Такой электронной почты не существует')
+            messages.error(request, "пользователь не найден.")
+            return redirect('login-url')
 
-
-
-        correct = user.check_password(password)
-
-        if correct == True:
+        if user.check_password(password):
             login(request, user)
             return redirect('home-url')
-        else:
-            return HttpResponse('Вы не правильно ввели пароль')
+
+        messages.error(request, "Неверный пароль.")
+        return redirect('login-url')
+
 
 class MakeLogoutView(View):
+    """
+    вьюшка для выхода из акк
+    """
     def post(self, request, *args, **kwargs):
         logout(request)
         return redirect('login-url')
